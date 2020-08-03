46390da76154d714173c4d35e344fecceb5d3b9b
Grace Spletzer <gracespletzer05@gmail.com>
2020-08-03 09:34:55 -0700

    leet code daily challenge aug 3

--- reverseInteger.js
+++ reverseInteger.js
@@ -3,12 +3,17 @@
  * @return {number}
  */
 var reverse = function (x) {
+  const limit = 2 ^ (31 - 1);
   //use a variable to hold the absolute value of the number
   const num = Math.abs(x);
-  //return the reversed&floated version of the absolute number and multiply by the sign of x
-  return Math.sign(x) * parseFloat(num.toString().split("").reverse().join(""));
+  //reversed&floated version of the absolute number and multiply by the sign of x
+  const reversed = parseFloat(num.toString().split("").reverse().join(""));
+
+  return reversed < limit ? Math.sign(x) * reversed : 0;
 };
 
 console.log(reverse(121));
 console.log(reverse(-3121));
 console.log(reverse(120));
+
+/* need to account for numbers that are larger than a 32 bit environment */

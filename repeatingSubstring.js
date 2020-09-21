9fb02445e0e7d8f928fbd59d6b829f11e57be348
Grace Spletzer <gracespletzer05@gmail.com>
2020-09-21 11:17:36 -0700

    wordPattern collab complete

--- repeatingSubstring.js
+++ repeatingSubstring.js
@@ -26,38 +26,38 @@ Explanation: It's the substring "abc" four times. (And the substring "abcabc" tw
  */
 
 //original attempt
-// var repeatedSubstringPattern = function (s) {
-//   //initialize variable for empty substring
-//   let subS = "";
-//   //initialize variable for result as false
-//   let result = false;
-//   //iterate through the string with a for loop until i < string length-1 (avoids false positive if no substring exists)
-//   for (let i = 0; i < s.length - 1; i++) {
-//     //add character at i to substring
-//     subS += s[i];
-//     //check if the length of the provided string divided by the substring returns 0
-//     if (s.length % subS.length === 0) {
-//       //if yes, then check if repeating substring by the number of string length/substring
-//       //equals the original string
-//       if (subS.repeat(s.length / subS.length) === s) {
-//         //if yes, change result to true and break loop
-//         result = true;
-//         break;
-//       }
-//       //else continue through loop
-//       else continue;
-//     }
-//     //else continue through loop
-//     else continue;
-//   }
-//   //return the result variable
-//   return result;
-// };
+var repeatedSubstringPattern = function (s) {
+  //initialize variable for empty substring
+  let subS = "";
+  //initialize variable for result as false
+  let result = false;
+  //iterate through the string with a for loop until i < string length-1 (avoids false positive if no substring exists)
+  for (let i = 0; i < s.length - 1; i++) {
+    //add character at i to substring
+    subS += s[i];
+    //check if the length of the provided string divided by the substring returns 0
+    if (s.length % subS.length === 0) {
+      //if yes, then check if repeating substring by the number of string length/substring
+      //equals the original string
+      if (subS.repeat(s.length / subS.length) === s) {
+        //if yes, change result to true and break loop
+        result = true;
+        break;
+      }
+      //else continue through loop
+      else continue;
+    }
+    //else continue through loop
+    else continue;
+  }
+  //return the result variable
+  return result;
+};
 
 //updated solution to only traverse half the given length
 //because minimum number of repetition that must occur to be true is 2
 //and refactor to reduce space complexity
-var repeatedSubstringPattern = function (s) {
+var repeatedSubstringPattern2 = function (s) {
   //initialize variable for empty substring
   let subS = "";
   //iterate through the string with a for loop until i < string length-1 (avoids false positive if no substring exists)
@@ -74,9 +74,9 @@ var repeatedSubstringPattern = function (s) {
   return false;
 };
 
-console.log(repeatedSubstringPattern("aaaa"));
-console.log(repeatedSubstringPattern("abcabcabcabc"));
-console.log(repeatedSubstringPattern("aabaabaaba"));
-console.log(repeatedSubstringPattern("abcabca"));
+console.log(repeatedSubstringPattern2("aaaa"));
+console.log(repeatedSubstringPattern2("abcabcabcabc"));
+console.log(repeatedSubstringPattern2("aabaabaab"));
+console.log(repeatedSubstringPattern2("abcabca"));
 
 //https://leetcode.com/problems/repeated-substring-pattern/discuss/550956/Two-JS-Solutions

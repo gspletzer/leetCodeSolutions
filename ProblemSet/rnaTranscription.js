363cd809802059c2f503666e18a05f36d63e3ef5
Grace Spletzer <gracespletzer05@gmail.com>
2021-03-05 17:42:47 -0800

    chore(rna): clean-up for pseudo code

--- ProblemSet/rnaTranscription.js
+++ ProblemSet/rnaTranscription.js
@@ -1,10 +1,19 @@
 /*
 Transcribes a character list representing DNA nucleotides to RNA
 
-C -> G
-G -> C
-A -> U
-T -> A
+Given a DNA strand, return its RNA complement (per RNA transcription).
+Both DNA and RNA strands are a sequence of nucleotides.
+
+The four nucleotides found in DNA are adenine (A), cytosine (C), guanine (G) and thymine (T).
+The four nucleotides found in RNA are adenine (A), cytosine (C), guanine (G) and uracil (U).
+
+Given a DNA strand, its transcribed RNA strand is formed by 
+replacing each nucleotide with its complement:
+
+  C -> G
+  G -> C
+  A -> U
+  T -> A
 
 ex: input = 'ACTG', output = 'UGAC'
  */
